17d33f0e93cf435e47ec575e5aaad2bec06ea8c3
Mohammad Sameer <sam0786.xyz@icloud.com>
2026-07-16 14:36:20 +0530

    chore: created APIRouter for further endpoints

---
+++ src/app/api/health.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from app.core.config import settings
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.get("/health")
+def health() -> dict:
+    return {
+        "status": "healthy",
+        "application": settings.app_name,
+        "version": settings.app_version,
+    }
